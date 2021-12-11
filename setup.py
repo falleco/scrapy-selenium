016c3a93033b5a6a0937faa90db027f9a77679fb
Rifqi AM <30653559+RifqiAM@users.noreply.github.com>
2021-12-11 16:50:44 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 
 from setuptools import setup, find_packages
 try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
 except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
-except ModuleNotFoundError:
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 def get_requirements(source):
     """Get the requirements from the given ``source``
